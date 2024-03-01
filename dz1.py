6c84e7e7e3fa524004324591f4b43c96e3640b74
Shulga Sergii <shulgapost@gmail.com>
2024-03-01 22:11:14 +0100

    Last

--- dz1.py
+++ dz1.py
@@ -1,44 +1,43 @@
-
+# реалізайія функціі для виведення списку колег, яких потрібно привітати з днем народження на тижні. 
 
 
 from datetime import datetime, timedelta
 from collections import defaultdict
 
 def get_birthdays_per_week(users):
-    today = datetime.today().date()
-    next_week_start = today + timedelta(days=(7 - today.weekday()))
-    next_week_end = next_week_start + timedelta(days=7)
-    
+    # Підготовка Даних
     birthdays = defaultdict(list)
     
+    # Отримання Поточної Дати
+    today = datetime.today().date()
+    
+    # Перебір Користувачів
     for user in users:
         name = user["name"]
-        birthday = user["birthday"].date()
+        birthday = user["birthday"].date()  # Конвертуємо час народження до типу date
         birthday_this_year = birthday.replace(year=today.year)
         
+        # Оцінка Дати на Цей Рік
         if birthday_this_year < today:
             birthday_this_year = birthday_this_year.replace(year=today.year + 1)
         
+        # Порівняння з Поточною Датою
         delta_days = (birthday_this_year - today).days
+        
+        # Визначення Дня Тижня
         if 0 <= delta_days < 7:
-            # Визначення дня тижня та перенесення на понеділок, якщо день народження вихідний
             birthday_weekday = (today + timedelta(days=delta_days)).strftime("%A")
             if birthday_weekday in ["Saturday", "Sunday"]:
-                birthday_weekday = "Monday"
-            # Збереження імені користувача за відповідним днем тижня
-            birthdays[birthday_weekday].append(name)
+                birthday_weekday = "Monday"  # Якщо це вихідний, переносимо на понеділок
+            birthdays[birthday_weekday].append(name)  # Зберігаємо ім'я користувача в відповідний день тижня
     
+    # Виведення Результату
     print("Birthdays for next week:")
     for day, names in birthdays.items():
         if names:
-            print(f"{day}: {', '.join(names)}")
+            print(f"{day}: {', '.join(names)}")  # Виводимо зібрані імена по днях тижня у відповідному форматі   
 
-# Приклад використання:
 users = [
-    {"name": "John Doe", "birthday": datetime(1985, 5, 15)},
-    {"name": "Jane Smith", "birthday": datetime(1990, 8, 25)},
-    {"name": "Alice Johnson", "birthday": datetime(1978, 9, 10)},
-    {"name": "Bob Brown", "birthday": datetime(1982, 4, 5)}
+    {"name": "Valerii Zaluzhnyi", "birthday": datetime(1973, 7, 8)}, {"name": "Kyrylo Budanov", "birthday": datetime(1986, 1, 4)}, {"name": "Alex Shein", "birthday": datetime(1983, 3, 5)},
 ]
-
 get_birthdays_per_week(users)
